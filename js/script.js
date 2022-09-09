c48c30a790d94b29e41a957276ae13c12511f0a6
rav4353 <ravanthsri20@gmail.com>
2022-09-09 15:33:05 +0530

    hi

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 var x = 9;
 
 for (i = 2; i <= x; i++) {
-var flag =0;
+    var flag = 0;
     for (j = 2; j <= i / 2; j++) {
         if (i % j == 0) {
-            flag=1;
+            flag = 1;
             break
         }
     }
